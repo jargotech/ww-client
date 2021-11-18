ca3825e67e0df5d469fdb2e60e93631bd69b151e
Anan Khan <anankhan@Jays-MacBook-Air.local>
2021-11-18 12:56:17 +0530

    pages created

---
+++ pages/car-detail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarDetail() {
+    return (
+        <div>
+            <h1>Car Detail</h1>
+        </div>
+    )
+}
---
+++ pages/car-listing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarListing() {
+    return (
+        <div>
+           <h1>Car Listing</h1> 
+        </div>
+    )
+}
