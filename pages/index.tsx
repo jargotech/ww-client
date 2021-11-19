f1c2bd87821fb716b89813ea6fc868c8e6fff146
Anan Khan <anankhan@Jays-MacBook-Air.local>
2021-11-19 19:35:28 +0530

    How it works component update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CtaBanner from '../src/components/CtaBanner'
 import OurStats from '../src/components/ourStats'
+import HowItWorks from '../src/components/howWorks'
 
 export default function Home() {
   return (
     <>
       <OurStats />
+      <HowItWorks />
       <CtaBanner />
     </>
   )
---
+++ src/components/howWorks.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Grid } from '@mui/material';
+import Image from 'next/image';
+import CreateLogo from '../../public/create.svg'
+
+export default function HowItWorks() {
+    return (
+        <section className="how-it-works">
+            <Container maxWidth="lg">
+                <h3>How it works</h3>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={4} sx={{ marginBottom: '15px' }}>
+                        <Image src={CreateLogo} height={160} width={200} alt="createLogo" />
+                        <h4>Browse our Collection</h4>
+                        <p>Create an account to get started.</p>
+                    </Grid>
+                    <Grid item xs={12} md={4} sx={{ marginBottom: '15px' }}>
+                        <Image src={CreateLogo} height={160} width={200} alt="createLogo" />
+                        <h4>Browse our Collection</h4>
+                        <p>Create an account to get started.</p>
+                    </Grid>
+                    <Grid item xs={12} md={4} sx={{ marginBottom: '15px' }}>
+                        <Image src={CreateLogo} height={160} width={200} alt="createLogo" />
+                        <h4>Browse our Collection</h4>
+                        <p>Create an account to get started.</p>
+                    </Grid>
+                </Grid>
+            </Container>
+        </section>
+    )
+}
