ce515106169735000259aeafcbccfa336607aa81
Shlok Chavan <shlok@pentaknot.com>
2022-02-18 21:16:28 +0530

    CTG

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-// export const APIURL = 'http://3.139.226.194:3000/api'
-export const APIURL = 'https://api.wishwheels.com/api'
+export const APIURL = 'http://3.139.226.194:3000/api'
+// export const APIURL = 'https://api.wishwheels.com/api'
 // export const APIURL = 'https://jsonplaceholder.typicode.com'
