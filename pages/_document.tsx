ac26fb69ddf43998166b45d274f8bc5fcd51b3d3
Shlok Chavan <shlok@pentaknot.com>
2022-03-11 19:51:52 +0530

    Inverted theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,8 @@ class MyDocument extends Document {
         <Head>
           {/* <title>Wish Wheels</title> */}
           {/* <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" /> */}
-          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-theme.png" />
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-theme.png" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-theme-inverted.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
           {/* <link rel="manifest" href="favicon/site.webmanifest" /> */}
